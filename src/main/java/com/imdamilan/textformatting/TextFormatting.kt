520babcebf855ac4e8721035054c09a8d431f298
ImDaMilan <milanmiljkovic007@gmail.com>
2022-07-21 12:35:05 +0200

    0.0.1

--- src/main/java/com/imdamilan/textformatting/TextFormatting.kt
+++ src/main/java/com/imdamilan/textformatting/TextFormatting.kt
@@ -3,11 +3,15 @@ package com.imdamilan.textformatting
 import com.imdamilan.textformatting.event.PlayerChatEvent
 import org.bstats.bukkit.Metrics
 import org.bukkit.Bukkit
+import org.bukkit.configuration.file.FileConfiguration
 import org.bukkit.plugin.java.JavaPlugin
 
 class TextFormatting : JavaPlugin() {
 
+    private var configFile: FileConfiguration? = null
+
     override fun onEnable() {
+        initConfig()
         Metrics(this, 15860)
         logger.info("Text Formatting is enabled!")
         Bukkit.getServer().pluginManager.registerEvents(PlayerChatEvent(), this)
@@ -15,5 +19,13 @@ class TextFormatting : JavaPlugin() {
 
     override fun onDisable() {
         logger.info("Text Formatting is disabled!")
+    }
+
+    private fun initConfig() {
+        this.saveDefaultConfig()
+        this.configFile = this.config
+        configFile!!.addDefault("autoupdate-enabled", true)
+        configFile!!.options().copyDefaults(true)
+        this.saveConfig()
     }
 }
