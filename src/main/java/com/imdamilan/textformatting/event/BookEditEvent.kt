457e4f3b2d99ea2eb57e6ad74fa50a97cf901a6a
ImDaMilan <milanmiljkovic007@gmail.com>
2022-07-21 21:38:44 +0200

    Added color/fonts to books

--- src/main/java/com/imdamilan/textformatting/event/BookEditEvent.kt
+++ src/main/java/com/imdamilan/textformatting/event/BookEditEvent.kt
@@ -5,14 +5,21 @@ import org.bukkit.ChatColor
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerEditBookEvent
+import org.bukkit.inventory.meta.BookMeta
 
 class BookEditEvent : Listener {
 
     @EventHandler
     fun onBookEdit(event: PlayerEditBookEvent) {
-        val meta = event.newBookMeta
-        meta.pages.replaceAll {
-            ChatColor.translateAlternateColorCodes(TextFormatting.configFile!!.getString("colorcode-prefix")!![0], it)
-        }
+        event.newBookMeta = format(event.newBookMeta)
+    }
+
+    private fun format(meta: BookMeta): BookMeta {
+        val pages = meta.pages
+        val new: MutableList<String> = ArrayList()
+        for (page in pages)
+            new.add(ChatColor.translateAlternateColorCodes(TextFormatting.configFile!!.getString("colorcode-prefix")!![0], page))
+        meta.pages = new
+        return meta
     }
 }
